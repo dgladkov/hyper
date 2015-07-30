86c9d54ebfbea0094a1a59ac4739fd395c7f9748
Dmitry Gladkov <dmitry.gladkov@gmail.com>
2015-07-30 16:55:37 +0300

    call the same public path and build path the same

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     },
     output: {
         path: path.join(__dirname, 'build'),
-        publicPath: '/assets/',
+        publicPath: '/build/',
         filename: 'bundle.js'
     },
     module: {
