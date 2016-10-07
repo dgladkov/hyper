0ad83324866092e93e0da9082f88a2d3a97761bb
Dmitry Gladkov <dmitry.gladkov@gmail.com>
2016-10-07 19:19:12 +0300

    ndash

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -31,7 +31,7 @@ export default class Playlist extends PureComponent {
             <ListItem
               key={index}
               onClick={this.handleItemClick.bind(this, index)}
-              primaryText={`${track.artist.name} - ${track.name}`}
+              primaryText={`${track.artist.name} – ${track.name}`}
               style={index === this.props.activeIndex ? STYLES.activeItem : null}
             />
           ))}
